6f533da87b9efd6d9d63bf8ec0b457346b1d64f0
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-10-22 07:43:37 +0700

    add codes

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('user_form');
+Route::get('tinhtong', function () {
+    return view('sum');
 });
+Route::post('tinhtong','CongController@tinhtong');
