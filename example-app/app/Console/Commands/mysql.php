213227915ce092f9c2ec1a53f55d90c2310819f9
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-10-29 09:31:27 +0700

    add file

--- example-app/app/Console/Commands/mysql.php
+++ example-app/app/Console/Commands/mysql.php
@@ -1,24 +1,25 @@
 <?php
+
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class dbcreate extends Command
+class mysql extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'db:create {name?}';
+    protected $signature = 'mysql:createdb {name?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create a new MySQL database based on the database config file or the provided name';
+    protected $description = 'Create a new mysql database schema based on the database config file';
 
     /**
      * Create a new command instance.
