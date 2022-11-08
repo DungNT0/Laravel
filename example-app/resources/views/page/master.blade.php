66e69c6ca2d0a3a9de35516f7d0c6c2436505667
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-11-08 19:40:58 +0700

    add file

--- example-app/resources/views/page/master.blade.php
+++ example-app/resources/views/page/master.blade.php
@@ -4,7 +4,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Laravel </title>
-    {{-- @push('styles') --}}
     <link href='http://fonts.googleapis.com/css?family=Dosis:300,400' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -15,7 +14,6 @@
 	<link rel="stylesheet" title="style" href="{{asset('source/assets/dest/css/style.css')}}">
 	<link rel="stylesheet" href="{{asset('source/assets/dest/css/animate.css')}}">
 	<link rel="stylesheet" title="style" href="{{asset('source/assets/dest/css/huong-style.css')}}">
-    {{-- @endpush --}}
 
 </head>
 <body>
